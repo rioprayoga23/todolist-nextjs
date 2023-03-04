28a63f3270582cac1693482eea28bc8e5e3dcd8a
Rio Prayoga <yoprayogap@gmail.com>
2023-03-04 19:51:53 +0700

    update pagination button

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -9,11 +9,19 @@ type Props = {
 const Pagination = ({ prevPage, nextPage, currentPage }: Props) => {
   return (
     <section className="flex mx-12 md:mx-20 px-5 py-3 rounded-full justify-between bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg text-white">
-      <button className="flex items-center" onClick={prevPage}>
+      <button
+        className="flex items-center"
+        onClick={prevPage}
+        disabled={currentPage === 1 ?? true}
+      >
         <ChevronLeft /> Prev
       </button>
       <p>{currentPage}</p>
-      <button className="flex items-center" onClick={nextPage}>
+      <button
+        className="flex items-center"
+        onClick={nextPage}
+        disabled={currentPage === 20 ?? true}
+      >
         Next <ChevronRight />
       </button>
     </section>
